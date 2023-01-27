39e27333c2a643a8727f77201dd73539b1c7b4f2
kingslayerrq <ruizhequ813@gmail.com>
2023-01-27 10:33:36 -0500

    started attempting room gen, fixed card manager & card Listen

---
+++ cardgameghub/Assets/Scripts/MapScripts/RoomNodes.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomNodes : MonoBehaviour
+{
+
+    public int maxRoomNum;
+    public float minRoomdist; // min distance between 2 rooms
+    public float maxRoomdist; // max distance between 2 rooms
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
